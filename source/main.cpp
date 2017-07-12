4b10158a7049e82b4f7f0e798b0a8401873e8c6e
muushi97 <moouushi@gmail.com>
2017-07-12 22:10:09 +0900

    共用体と列挙体を用いた，整数型変数によるデータ管理テスト

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,62 @@
 #include <cstdio>
+#include <cstdint>
 #include <iostream>
+#include <vector>
+#include <string>
+
+namespace cut_cut_cut
+{
+	enum item_type : uint_least64_t
+	{
+		no,
+		constant,
+		variable,
+		function,
+	};
+	enum function_type : uint_least64_t
+	{
+		non,
+		add,
+		sub,
+		mul,
+		div,
+	};
+
+	union convert_union
+	{
+		uint_least64_t _integer;
+		double _decimal;
+		item_type _item_type;
+		function_type _function_type;
+	};
+
+	class item_parameter
+	{
+	protected:
+		item_type m_type;
+		std::vector<uint_least64_t> m_data;
+
+	public:
+		item(item_type type)
+		{
+			m_type = type;
+		}
+	};
+
+	class item_interface
+	{
+	public:
+		uint_least64_t hoge;
+	};
+}
 
 int main()
 {
-	std::cout << "hoge" << std::endl;
+	cut_cut_cut::convert_union hoge;
+
+	hoge._item_type = cut_cut_cut::item_type::function;
+
+	std::cout << hoge._integer << std::endl;
 
 	return 0;
 }
